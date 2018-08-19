500a61c7ccb92a2dfb15d98d816ef352ebad3083
Patrick Birnzain <patrick.birnzain@digividual.at>
2018-08-19 19:47:52 +0200

    Add webapp

---
+++ pwa/exocortex/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  baseUrl: '/static/app/',
+  outputDir: 'build',
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
