62c8270320c150a9a9d0d81067989fbacdbd1e35
Patrick Birnzain <patrick.birnzain@digividual.at>
2019-02-03 17:25:50 +0100

    Add empty state area to Edit view

--- pwa/src/views/Edit.vue
+++ pwa/src/views/Edit.vue
@@ -1,20 +1,22 @@
 <template lang="pug">
-  .edit-topic(v-if="selectedTopic")
+  .edit-topic
     v-toolbar
       router-link(:to="{ name: 'home', params: {}}")
         v-icon arrow_back
 
-      v-text-field(ref="tf" :autofocus="true" single-line full-width hide-details :value="selectedTopic.title" @change="onTitleChanged")
-
-      a(@click="onDelete")
-        v-icon delete
+      template(v-if="selectedTopic")
+        v-text-field(ref="tf" :autofocus="true" single-line full-width hide-details :value="selectedTopic.title" @change="onTitleChanged")
+        a(@click="onDelete")
+          v-icon delete
 
     v-container
-      topic(:topic="selectedTopic" @topic-changed="onTopicChanged" :hideTitle="true")
+      topic(v-if="selectedTopic" :topic="selectedTopic" @topic-changed="onTopicChanged" :hideTitle="true")
+      empty-state(v-else tagline="Topic not found." message="Most likely it has been deleted.")
 </template>
 <script>
 import { VBtn, VContainer, VToolbar, VToolbarTitle, VSpacer, VIcon, VTextField } from 'vuetify/lib'
-import Topic from '../components/Topic'
+import Topic from '@/components/Topic'
+import EmptyState from '@/components/EmptyState'
 
 export default {
   components: {
@@ -25,18 +27,12 @@ export default {
     VToolbarTitle,
     VSpacer,
     VIcon,
-    VTextField
+    VTextField,
+    EmptyState
   },
   computed: {
     selectedTopic () {
       return this.$store.getters['topics/selectedTopic']
-    }
-  },
-  watch: {
-    selectedTopic (newSelectedTopic) {
-      if (!newSelectedTopic) {
-        this.$router.push('/')
-      }
     }
   },
   mounted () {
@@ -65,14 +61,25 @@ export default {
     },
     onDelete () {
       this.$store.dispatch('topics/delete', this.selectedTopic)
+      this.$router.push('/')
     }
   }
 }
 </script>
 <style lang="scss">
 .edit-topic {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
   background: white;
 
+  .container {
+    position: relative;
+  }
+  .v-toolbar {
+    z-index: 10;
+  }
   .v-toolbar__title {
     margin-left: 20px;
   }
