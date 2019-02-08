a4f1e627ca63c9253ab6297561a551da5d1d590c
Patrick Birnzain <patrick.birnzain@digividual.at>
2019-02-08 14:46:39 +0100

    Remove old ui app

--- exocortex/settings.py
+++ exocortex/settings.py
@@ -16,7 +16,6 @@ import socket
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -43,7 +42,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'channels',
     'store',
-    'ui',
     'pwa',
 ]
 
