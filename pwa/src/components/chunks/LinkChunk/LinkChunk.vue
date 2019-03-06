84b0cabe7506fc6ee6ea55d9e3bed8f0dd0a6fff
Patrick Birnzain <patrick.birnzain@digividual.at>
2019-03-06 20:10:34 +0100

    Style linkchunk like textchunk

--- pwa/src/components/chunks/LinkChunk/LinkChunk.vue
+++ pwa/src/components/chunks/LinkChunk/LinkChunk.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  v-card.links(v-if="links")
+  v-card.links.markdown-body(v-if="links")
     h1 References
     template(v-for="link of links")
       individual-link(:link="link")
