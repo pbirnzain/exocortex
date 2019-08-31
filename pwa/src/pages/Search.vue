81add65268ea1b3c9c587b1c759820b1a48c9d1b
Patrick Birnzain <patrick.birnzain@digividual.at>
2019-08-31 23:14:48 +0200

    Add OpenSearch POC

---
+++ pwa/src/pages/Search.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+</template>
+
+<script>
+export default {
+  mounted () {
+    const searchText = this.$route.query.text
+    if (searchText) {
+      this.$store.dispatch('search/setSearchText', searchText)
+      this.$store.dispatch('search/setFilter', 'open')
+    }
+
+    this.$router.push(`/`)
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
