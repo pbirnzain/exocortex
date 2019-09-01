4e7341b0afcb256131e8574aff7354d98d6d5691
Patrick Birnzain <patrick.birnzain@digividual.at>
2019-09-01 06:16:09 +0200

    Add additional colors for topic state to theme

--- pwa/src/plugins/vuetify.js
+++ pwa/src/plugins/vuetify.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuetify, { VToolbar, VToolbarTitle, VSpacer, VIcon,
   VTextField, VBtn, VProgressCircular, VContainer } from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
+import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
   // eslint object-property-newline: "off"
@@ -16,5 +17,10 @@ Vue.use(Vuetify, {
     VContainer
   },
   customProperties: true,
-  iconfont: 'md'
+  iconfont: 'md',
+  theme: {
+    inactive: colors.grey.darken1,
+    overdue: colors.red,
+    complete: colors.green
+  }
 })
