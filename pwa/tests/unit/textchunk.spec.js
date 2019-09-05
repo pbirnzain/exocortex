09be66b94bb15c5fc456fea51d0aed97d7e6508e
Patrick Birnzain <patrick.birnzain@digividual.at>
2019-09-05 16:45:31 +0200

    Fix textchunk unit tests

--- pwa/tests/unit/textchunk.spec.js
+++ pwa/tests/unit/textchunk.spec.js
@@ -2,25 +2,28 @@ import { mount } from '@vue/test-utils'
 import TextChunk from '@/pages/edit/chunks/TextChunk.vue'
 
 describe('TextChunk.vue', () => {
-  it('renders markdown', () => {
+  let wrapper
+
+  beforeEach(async () => {
     const chunk = { text: '## ThisIsMyChunkText' }
-    const wrapper = mount(TextChunk, {
+    wrapper = mount(TextChunk, {
       propsData: { chunk }
     })
+    await wrapper.vm.$nextTick()
+  })
+
+  it('renders markdown', async () => {
     expect(wrapper.find('.markdown-body').text()).toContain('ThisIsMyChunkText')
     expect(wrapper.find('.markdown-body').contains('h2')).toBe(true)
   })
 
-  it('allows editing on click', () => {
-    const chunk = { text: '## ThisIsMyChunkText' }
-    const wrapper = mount(TextChunk, {
-      propsData: { chunk }
-    })
-
+  it('allows editing on click', async () => {
     wrapper.find('.markdown-body').trigger('click')
+    await wrapper.vm.$nextTick()
     expect(wrapper.find('textarea').element.value).toContain('## ThisIsMyChunkText')
 
     wrapper.find('textarea').trigger('blur')
+    await wrapper.vm.$nextTick()
     expect(wrapper.find('.markdown-body').contains('h2')).toBe(true)
   })
 })
